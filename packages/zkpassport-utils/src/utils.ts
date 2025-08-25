84a22cd1351ef58b5d3b6b235550ef48480ce019
Michael Elliot <michael@digital.com.au>
2025-08-25 22:59:57 +0800

    feat(utils): Add sanctions utils (#57)
    * chore: ofac proof utils
    
    * ofac -> sanctions
    
    * chore: remove magic numbers in circuit-matcher.test.ts
    
    * chore: circuit matcher test
    
    * fix tests
    
    * style: formatting
    
    * fix build
    
    * feat: add serialize and loadFromSerialized to AsyncIMT
    
    * feat(utils): ordered mt (#65)
    
    * feat: ordered mt
    
    * chore: add note
    
    * chore: consolidate into one tree
    
    * fix
    
    * fix
    
    * fmt
    
    * fix: tidy up sanctions proof formatting function
    
    * fix
    
    * fmt
    
    * fmt
    
    * chore: reorder
    
    * fix: review comment
    
    ---------
    
    Co-authored-by: Maddiaa0 <47148561+Maddiaa0@users.noreply.github.com>

--- packages/zkpassport-utils/src/utils.ts
+++ packages/zkpassport-utils/src/utils.ts
@@ -203,6 +203,20 @@ export function packBeBytesIntoFields(bytes: Uint8Array, maxChunkSize: number): 
   return result
 }
 
+/**
+ * Converts a string (or string array) into a hex string array
+ * Useful for going from text to unicode
+ * @param str
+ * @returns
+ */
+export function stringToAsciiStringArray(str: string): string[] {
+  const asciiStringArray = []
+  for (let i = 0; i < str.length; i++) {
+    asciiStringArray.push(BigInt(str.charCodeAt(i)))
+  }
+  return asciiStringArray.map((b) => `0x${b.toString(16)}`)
+}
+
 export function getIdFromChain(chain: SupportedChain): number {
   if (chain === "ethereum_sepolia") {
     return 11155111
