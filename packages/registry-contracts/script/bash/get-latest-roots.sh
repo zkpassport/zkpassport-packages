9a290440a8081d1efc36a3a9fe71d985b8e42722
Maddiaa <47148561+Maddiaa0@users.noreply.github.com>
2025-08-26 11:59:20 +0100

    chore(registry): Add sanctions registry (#72)
    * reafactor: inherit RegistryInstance
    
    * fix: update tests
    
    * fix: accidentally commited sanctions reg
    
    * chore: named import
    
    * chore: add sanctions registry
    
    * chore: add deployment script
    
    * chore: add sanctions registry to update_roots

--- packages/registry-contracts/script/bash/get-latest-roots.sh
+++ packages/registry-contracts/script/bash/get-latest-roots.sh
@@ -7,6 +7,7 @@ export RPC_URL=${RPC_URL:-http://localhost:8545}
 
 CERTIFICATE_REGISTRY_ID=0x0000000000000000000000000000000000000000000000000000000000000001
 CIRCUIT_REGISTRY_ID=0x0000000000000000000000000000000000000000000000000000000000000002
+SANCTIONS_REGISTRY_ID=0x0000000000000000000000000000000000000000000000000000000000000003
 
 ROOT_REGISTRY_ADDRESS=$(cat broadcast/DeployRootRegistry.s.sol/$CHAIN_ID/run-latest.json | jq -r '.transactions[] | select(.transactionType=="CREATE") | .contractAddress')
 echo "Using Root Registry at: $ROOT_REGISTRY_ADDRESS"
@@ -16,6 +17,9 @@ echo "Certificate Registry latest root: $LATEST_ROOT"
 
 LATEST_ROOT=$(cast call $ROOT_REGISTRY_ADDRESS "latestRoot(bytes32)" $CIRCUIT_REGISTRY_ID --rpc-url $RPC_URL)
 echo "Circuit Registry latest root: $LATEST_ROOT"
+
+LATEST_ROOT=$(cast call $ROOT_REGISTRY_ADDRESS "latestRoot(bytes32)" $SANCTIONS_REGISTRY_ID --rpc-url $RPC_URL)
+echo "Sanctions Registry latest root: $LATEST_ROOT"
 
 REGISTRY_HELPER_ADDRESS=$(cat broadcast/DeployRegistryHelper.s.sol/$CHAIN_ID/run-latest.json | jq -r '.transactions[] | select(.transactionType=="CREATE") | .contractAddress')
 echo "Using Registry Helper at: $REGISTRY_HELPER_ADDRESS"
