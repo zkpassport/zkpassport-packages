392acc93adeb8cd0e17db0c2d1a43e10dbcc80e1
Michael Elliot <michael@digital.com.au>
2025-10-30 00:09:12 +0800

    turbo power

---
+++ packages/zkpassport-sdk/tsup.config.esm.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "tsup"
+
+export default defineConfig({
+  entry: ["src/**/*.ts", "!src/**/*.test.ts"],
+  format: "esm",
+  outDir: "dist/esm",
+  outExtension: () => ({ js: ".js" }),
+  dts: false, // Types are handled by tsc -b
+  clean: true,
+  splitting: false,
+  sourcemap: true,
+  treeshake: true,
+  external: [
+    // Externalize workspace dependencies for release builds
+    "@zkpassport/utils",
+    "@zkpassport/registry",
+  ],
+})
