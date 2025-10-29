5c1937bc7c2d115b7766f0e8d8951e258f23692d
Michael Elliot <michael@digital.com.au>
2025-10-29 22:58:05 +0800

    fix registry-explorer build

--- packages/registry-explorer/next.config.js
+++ packages/registry-explorer/next.config.js
@@ -1,3 +1,5 @@
+const path = require("path")
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config, {}) => {
@@ -13,6 +15,17 @@ const nextConfig = {
       test: /\.wasm$/,
       type: "asset/resource",
     })
+
+    // Add alias resolution for @zkpassport/utils package
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@/utils": path.resolve(__dirname, "../zkpassport-utils/src/utils"),
+      "@/merkle-tree": path.resolve(__dirname, "../zkpassport-utils/src/merkle-tree"),
+      "@/types": path.resolve(__dirname, "../zkpassport-utils/src/types"),
+      "@/passport": path.resolve(__dirname, "../zkpassport-utils/src/passport"),
+      "@/circuits": path.resolve(__dirname, "../zkpassport-utils/src/circuits"),
+      "@/index": path.resolve(__dirname, "../zkpassport-utils/src/index"),
+    }
 
     return config
   },
