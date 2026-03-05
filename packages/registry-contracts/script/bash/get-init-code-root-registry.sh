94550b7b50fd04856169918c8ed3a7c9b80178d4
Michael Elliot <michael@digital.com.au>
2026-03-06 01:21:13 +0800

    simplify get-init-code scripts

--- packages/registry-contracts/script/bash/get-init-code-root-registry.sh
+++ packages/registry-contracts/script/bash/get-init-code-root-registry.sh
@@ -10,24 +10,23 @@ GUARDIAN="0x0000000000000000000000000000000000000000"
 ARGS=("$ADMIN" "$GUARDIAN")
 
 echo "Contract name: $CONTRACT_NAME"
-echo "Constructor signature: $CONSTRUCTOR_SIGNATURE"
+echo "Constructor sig: $CONSTRUCTOR_SIGNATURE"
+echo "Constructor args: ${ARGS[@]}"
+echo
 echo "Admin: $ADMIN"
 echo "Guardian: $GUARDIAN"
-echo "Args: ${ARGS[@]}"
-
-# Calculate the init code
+echo
 echo "Calculating init code..."
-forge build --force src/$CONTRACT_NAME.sol
+
+echo "Encoding constructor args: cast abi-encode \"$CONSTRUCTOR_SIGNATURE\" \"${ARGS[@]}\""
+CONSTRUCTOR_ARGS=$(cast abi-encode "$CONSTRUCTOR_SIGNATURE" "${ARGS[@]}")
+echo "Constructor args (ABI encoded): $CONSTRUCTOR_ARGS"
 
 echo "Getting contract bytecode..."
+forge build --force src/$CONTRACT_NAME.sol
 BYTECODE=$(forge inspect $CONTRACT_NAME bytecode)
 
-echo "Encoding constructor arguments..."
-echo cast abi-encode "$CONSTRUCTOR_SIGNATURE" "${ARGS[@]}"
-CONSTRUCTOR_ARGS=$(cast abi-encode "$CONSTRUCTOR_SIGNATURE" "${ARGS[@]}")
-
 echo "Combining bytecode with constructor args..."
-# Remove the 0x prefix from constructor args and append to bytecode
 INIT_CODE="${BYTECODE}${CONSTRUCTOR_ARGS:2}"
 INIT_CODE_HASH=$(cast keccak $INIT_CODE)
 
@@ -35,7 +34,7 @@ echo "Init code: $INIT_CODE"
 echo "Init code hash: $INIT_CODE_HASH"
 
 EXPECTED_INIT_CODE_HASH="0xd3b588d8ef7ca9ea5e601ff345094cbc376dafa2e25c5ab4d0c5238bcaeda586"
-# If INIT_CODE_HASH is not equal to the expected hash, print an error and exit with an error code
+# Error if INIT_CODE_HASH not equal to expected hash
 if [ "$INIT_CODE_HASH" != "$EXPECTED_INIT_CODE_HASH" ]; then
     echo "Error: Init code hash does not match expected hash"
     exit 1
--- packages/registry-contracts/script/bash/get-init-code-root-verifier.sh
+++ packages/registry-contracts/script/bash/get-init-code-root-verifier.sh
@@ -11,25 +11,24 @@ ROOT_REGISTRY="0x1D0000020038d6E40E1d98e09fA1bb3A7DAA8B70"
 ARGS=("$ADMIN" "$GUARDIAN" "$ROOT_REGISTRY")
 
 echo "Contract name: $CONTRACT_NAME"
-echo "Constructor signature: $CONSTRUCTOR_SIGNATURE"
+echo "Constructor sig: $CONSTRUCTOR_SIGNATURE"
+echo "Constructor args: ${ARGS[@]}"
+echo
 echo "Admin: $ADMIN"
 echo "Guardian: $GUARDIAN"
-echo "Root registry: $ROOT_REGISTRY"
-echo "Args: ${ARGS[@]}"
-
-# Calculate the init code
+echo "RootRegistry: $ROOT_REGISTRY"
+echo
 echo "Calculating init code..."
-forge build --force src/$CONTRACT_NAME.sol
+
+echo "Encoding constructor args: cast abi-encode \"$CONSTRUCTOR_SIGNATURE\" \"${ARGS[@]}\""
+CONSTRUCTOR_ARGS=$(cast abi-encode "$CONSTRUCTOR_SIGNATURE" "${ARGS[@]}")
+echo "Constructor args (ABI encoded): $CONSTRUCTOR_ARGS"
 
 echo "Getting contract bytecode..."
+forge build --force src/$CONTRACT_NAME.sol
 BYTECODE=$(forge inspect $CONTRACT_NAME bytecode)
 
-echo "Encoding constructor arguments..."
-echo cast abi-encode "$CONSTRUCTOR_SIGNATURE" "${ARGS[@]}"
-CONSTRUCTOR_ARGS=$(cast abi-encode "$CONSTRUCTOR_SIGNATURE" "${ARGS[@]}")
-
 echo "Combining bytecode with constructor args..."
-# Remove the 0x prefix from constructor args and append to bytecode
 INIT_CODE="${BYTECODE}${CONSTRUCTOR_ARGS:2}"
 INIT_CODE_HASH=$(cast keccak $INIT_CODE)
 
@@ -37,7 +36,7 @@ echo "Init code: $INIT_CODE"
 echo "Init code hash: $INIT_CODE_HASH"
 
 EXPECTED_INIT_CODE_HASH="0xf3fd822c2cb27cea5f246e0742385b562b64ebb12e0818d18185072fb22961b0"
-# If INIT_CODE_HASH is not equal to the expected hash, print an error and exit with an error code
+# Error if INIT_CODE_HASH not equal to expected hash
 if [ "$INIT_CODE_HASH" != "$EXPECTED_INIT_CODE_HASH" ]; then
     echo "Error: Init code hash does not match expected hash"
     exit 1
