5b8a2d6c655544fc1fd2d66ef9583af7a9acaa44
Michael Elliot <michael@digital.com.au>
2026-03-12 22:48:12 +0800

    add natspec comments to SubVerifier

--- packages/registry-contracts/src/SubVerifier.sol
+++ packages/registry-contracts/src/SubVerifier.sol
@@ -21,8 +21,11 @@ contract SubVerifier {
     address public admin;
     bool public paused;
 
+    // The address of the root verifier
     RootVerifier public rootVerifier;
 
+    // Mapping from vkey hash of each outer circuit to its proof verifier (UltraHonk verifier) address
+    // Maps use the vkeyHash of the outer circuit to the ProofVerifier contract address
     mapping(bytes32 => address) public proofVerifiers;
 
     event SubVerifierDeployed(address indexed admin, address indexed rootVerifier);
@@ -31,6 +34,11 @@ contract SubVerifier {
     event ProofVerifierRemoved(address indexed proofVerifier, bytes32 indexed vkeyHash);
     event PausedStatusChanged(bool paused);
 
+    /**
+     * @dev Constructor
+     * @param _admin The admin address
+     * @param _rootVerifier The address of the ZKPassport root verifier
+     */
     constructor(address _admin, RootVerifier _rootVerifier) {
         require(_admin != address(0), "Admin cannot be zero address");
         admin = _admin;
@@ -91,12 +99,22 @@ contract SubVerifier {
         return DateUtils.isDateValid(currentDateTimeStamp, validityPeriodInSeconds);
     }
 
+    /**
+     * @notice Verifies that the proof was generated for the given scope (domain) and subscope (service scope)
+     * @param publicInputs The public inputs of the proof
+     * @param scope The scope (domain) to check against
+     * @param subscope The subscope (service scope) to check against
+     * @return True if valid, false otherwise
+     */
     function _verifyScopes(bytes32[] calldata publicInputs, string calldata scope, string calldata subscope)
         internal
         pure
         returns (bool)
     {
+        // One byte is dropped at the end
+        // What we call scope internally is derived from the domain
         bytes32 scopeHash = StringUtils.isEmpty(scope) ? bytes32(0) : sha256(abi.encodePacked(scope)) >> 8;
+        // What we call the subscope internally is the service scope specified manually in the SDK
         bytes32 subscopeHash = StringUtils.isEmpty(subscope) ? bytes32(0) : sha256(abi.encodePacked(subscope)) >> 8;
         return
             publicInputs[PublicInput.SCOPE_INDEX] == scopeHash
@@ -107,12 +125,19 @@ contract SubVerifier {
         uint256 offset = 0;
         uint256 index = 0;
         while (offset < committedInputs.length && index < paramCommitments.length) {
+            // The committed inputs are formatted as follows:
+            // - 1 byte: proof type
+            // - 2 bytes: length of the committed inputs
+            // - N bytes: committed inputs for a given proof
             uint16 length = uint16(bytes2(committedInputs[offset + 1:offset + 3]));
+            // One byte is dropped inside the circuit as BN254 is limited to 254 bits
+            // We also add 3 bytes to take into account the proof type and length
             bytes32 calculatedCommitment = sha256(abi.encodePacked(committedInputs[offset:offset + length + 3])) >> 8;
             require(calculatedCommitment == paramCommitments[index], "Invalid commitment");
             offset += length + 3;
             index++;
         }
+        // Check that all the committed inputs have been covered, otherwise something is wrong
         require(offset == committedInputs.length, "Invalid committed inputs length");
         require(index == paramCommitments.length, "Invalid parameter commitments");
     }
@@ -137,6 +162,14 @@ contract SubVerifier {
         require(_rootRegistry.isRootValid(RegistryID.CIRCUIT, circuitRoot, timestamp), "Invalid circuit registry root");
     }
 
+    /**
+     * @notice Verifies a ZKPassport proof
+     * @dev This function is called by the root verifier to verify a proof for a specific version
+     * @param rootRegistry The root registry
+     * @param params The proof verification parameters
+     * @return isValid True if the proof is valid, false otherwise
+     * @return uniqueIdentifier The unique identifier associated to the identity document that generated the proof
+     */
     function verify(RootRegistry rootRegistry, ProofVerificationParams calldata params)
         external
         view
@@ -144,27 +177,38 @@ contract SubVerifier {
         onlyRootVerifier
         returns (bool isValid, bytes32 uniqueIdentifier)
     {
+        // Get the verifier for the Outer Circuit corresponding to the vkey hash
         address verifier = _getProofVerifier(params.proofVerificationData.vkeyHash);
 
         uint256 currentTimestamp = uint256(params.proofVerificationData.publicInputs[PublicInput.CURRENT_DATE_INDEX]);
 
+        // Validate certificate registry root
         _validateCertificateRoot(
             rootRegistry,
             params.proofVerificationData.publicInputs[PublicInput.CERTIFICATE_REGISTRY_ROOT_INDEX],
             currentTimestamp
         );
 
+        // Validate circuit registry root
         _validateCircuitRoot(
             rootRegistry,
             params.proofVerificationData.publicInputs[PublicInput.CIRCUIT_REGISTRY_ROOT_INDEX],
             currentTimestamp
         );
 
+        // Checks the date of the proof
+        // This is the current date used as public input in the disclosure proofs
+        // so verifying it here guarantees that the disclosure proofs were generated with this date
         require(
             _checkDateValidity(currentTimestamp, params.serviceConfig.validityPeriodInSeconds),
             "The proof was generated outside the validity period"
         );
 
+        // Validate scopes
+        // It is recommended to verify this against static variables in your contract
+        // by calling `helper.verifyScopes(publicInputs, domain, scope)` or setting the domain and scope
+        // in the params inside your smart contract function before calling `verifier.verify(params)`
+        // Check SampleContract.sol for an example
         require(
             _verifyScopes(
                 params.proofVerificationData.publicInputs, params.serviceConfig.domain, params.serviceConfig.scope
@@ -172,7 +216,9 @@ contract SubVerifier {
             "Invalid domain or scope"
         );
 
+        // Verifies the commitments against the committed inputs
         _verifyCommittedInputs(
+            // Extracts the commitments from the public inputs
             params.proofVerificationData
             .publicInputs[PublicInput.PARAM_COMMITMENTS_INDEX:params.proofVerificationData.publicInputs.length - 2],
             params.committedInputs
@@ -182,23 +228,32 @@ contract SubVerifier {
             uint256(params.proofVerificationData.publicInputs[params.proofVerificationData.publicInputs.length - 2])
         );
 
+        // Allow mock proofs in dev mode
+        // Note: On mainnets, this stage won't be reached as the ZKR certificates will not be part
+        // of the mainnet registries and the verification will fail at _validateCertificateRoot
         require(
             (nullifierType != NullifierType.NON_SALTED_MOCK_NULLIFIER
                     && nullifierType != NullifierType.SALTED_MOCK_NULLIFIER) || params.serviceConfig.devMode,
             "Mock proofs are only allowed in dev mode"
         );
 
+        // For now, only non-salted nullifiers are supported
+        // but salted nullifiers can be used in dev mode
+        // They will be later once a proper registration mechanism is implemented
         require(
             nullifierType == NullifierType.NON_SALTED_NULLIFIER || params.serviceConfig.devMode,
             "Salted nullifiers are not supported for now"
         );
 
+        // Call the proof verifier for the given Outer Circuit to verify if the actual proof is valid
         isValid = IProofVerifier(verifier)
             .verify(params.proofVerificationData.proof, params.proofVerificationData.publicInputs);
 
+        // Get the unique identifier from the public inputs
         uint256 uniqueIdentifierIndex = params.proofVerificationData.publicInputs.length - 1;
         uniqueIdentifier = params.proofVerificationData.publicInputs[uniqueIdentifierIndex];
 
+        // Return the validity of the proof verification and the unique identifier
         return (isValid, uniqueIdentifier);
     }
 }
