43f49a7b1eef78542a9dea65fcf644c43bf09bda
saleel <saleel@saleel.xyz>
2026-09-15 21:40:08 +0400

    chore: update deploy script and deploy

--- packages/onchain-credentials/contracts/script/DeployZKPassportCredentials.s.sol
+++ packages/onchain-credentials/contracts/script/DeployZKPassportCredentials.s.sol
@@ -14,14 +14,16 @@ contract DeployZKPassportCredentialsScript is Script {
         address adminAddress = vm.envAddress("ZKPASSPORT_CREDENTIALS_ADMIN_ADDRESS");
         require(adminAddress != address(0), "ZKPASSPORT_CREDENTIALS_ADMIN_ADDRESS must be set");
         bytes32 create2Salt = vm.envOr("CREATE2_SALT", bytes32(0));
+        bytes32 evaluatorSalt = vm.envOr("EVALUATOR_SALT", create2Salt);
+        bytes32 credentialsSalt = vm.envOr("CREDENTIALS_SALT", create2Salt);
         // Dev-mode evaluators accept mock-document proofs: testnets only, never mainnet.
         bool devMode = vm.envOr("ZKPASSPORT_CREDENTIALS_DEV_MODE", false);
 
         vm.startBroadcast();
         PolicyEvaluatorV1 policyEvaluator =
-            new PolicyEvaluatorV1{salt: create2Salt}(IRootVerifier(rootVerifier), devMode);
+            new PolicyEvaluatorV1{salt: evaluatorSalt}(IRootVerifier(rootVerifier), devMode);
         ZKPassportCredentials zkPassportCredentials =
-            new ZKPassportCredentials{salt: create2Salt}(domain, adminAddress, policyEvaluator);
+            new ZKPassportCredentials{salt: credentialsSalt}(domain, adminAddress, policyEvaluator);
         vm.stopBroadcast();
 
         console.log("ZKPassportCredentials deployed at:", address(zkPassportCredentials));
@@ -36,3 +38,18 @@ contract DeployZKPassportCredentialsScript is Script {
         vm.writeJson(json, string.concat("deployments/", vm.toString(block.chainid), ".json"));
     }
 }
+
+/**
+ *   Usage:
+ *   In zkpassport-packages/packages/onchain-credentials/contracts
+ *
+ *   export ROOT_VERIFIER_ADDRESS=0x1D000001000EFD9a6371f4d90bB8920D5431c0D8
+ *   export ZKPASSPORT_CREDENTIALS_DOMAIN=zkpassport.id
+ *   export ZKPASSPORT_CREDENTIALS_ADMIN_ADDRESS=0x2000ab040a899f914D6DfD2457C3dFBB22d4c762
+ *   export ZKPASSPORT_CREDENTIALS_DEV_MODE=false
+ *   export EVALUATOR_SALT=0xaff5912563440cf851f9f0bfe451f004a1cf59dc58a700ae23548a008602e8ca
+ *   export CREDENTIALS_SALT=0xa0f75792a261027209ba68bf657a4e5b7a2d8ebe0804382920b59e2e9ff617bc
+ *
+ *   forge script script/DeployZKPassportCredentials.s.sol --rpc-url <> --private-key <> --broadcast --verify --etherscan-api-key <>
+ *
+ */
